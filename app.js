cc44e667b2778d3cd27878980028a395714538c2
bhandariabhishek488@gmail.com <bhaskarbhandari372@gmail.com>
2023-07-31 14:28:35 +0530

    hvyhg

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-
-const parent=React.createElement(
-    "div", 
-    {id: "parent"}, 
-    [React.createElement("div", {id: "child"}, 
-    [React.createElement("h1",{}, "i am an h1 tag"), 
-    React.createElement("h2",{}, "i am an h2 tag")])],
-    [React.createElement("div", {id: "child"}, 
-    [React.createElement("h1",{}, "i am an h1 tag"), 
-    React.createElement("h2",{}, "i am an h2 tag")])] 
-);
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
+import React from "react"
+import ReactDOM from "react-dom/client";
+/* Heder
+    - Logo
+    - Nav-items
+Body
+    - Search 
+    - Restaurantbox
+        - RestaurantCard
+Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+*/
+const Header = () => {
+    return (
+        <>
+        <div className="navbar">
+            <div className="navitems flex">
+                <img src="" alt=""/>
+                <ul className="flex">
+                    <li className="font-bold">home</li>
+                    <li className="font-bold">about us</li>
+                    <li className="font-bold">contact us</li>
+                    <li className="font-bold">cart</li>
+                </ul>
+            </div>
+        </div>
+        </>
+    )
+}
+const AppLayout = () => {
+    return(
+        <>
+            <div className="app">
+                <Header/>
+            </div>
+        </>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
